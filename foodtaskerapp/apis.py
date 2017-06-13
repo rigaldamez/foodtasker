a522e7a1a0b196bcc20b546c0a2e5c0fdbb0348c
Rogelio I Galdamez <rigaldamez@Rogelios-MacBook-Pro.local>
2017-06-13 16:20:17 +0930

    task 12 complete

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -7,7 +7,11 @@ from django.views.decorators.csrf import csrf_exempt
 from oauth2_provider.models import AccessToken
 
 from foodtaskerapp.models import Restaurant, Meal, Order, OrderDetails
-from foodtaskerapp.serializers import RestaurantSerializer, MealSerializer
+from foodtaskerapp.serializers import RestaurantSerializer, MealSerializer, OrderSerializer
+
+################
+# CUSTOMER APIS
+################
 
 #This is for a Customer to get all the Restaurants from the DB returned as a JSON objects
 def customer_get_restaurants(request):
@@ -86,10 +90,126 @@ def customer_add_order(request):
 
 #Get the latest order
 def customer_get_latest_order(request):
+    #get token
+    access_token = AccessToken.objects.get(token = request.GET.get("access_token"), expires__gt = timezone.now())
 
-    return JsonResponse({})
+    #get Customer
+    customer = access_token.user.customer
+
+    #get Order
+    order = OrderSerializer(Order.objects.filter(customer = customer).last()).data
+
+    return JsonResponse({"order": order})
+
+
+################
+# RESTAURANT APIS
+################
 
 def restaurant_order_notification(request, last_request_time):
     notification = Order.objects.filter(restaurant = request.user.restaurant, created_at__gt = last_request_time).count()
 
     return JsonResponse({"notification": notification})
+
+
+
+################
+# DRIVER APIS
+################
+
+#Gets all the orders that are ready for delivery
+def driver_get_ready_orders(request):
+    #get all the orders that are READY and no DRIVER assigned
+    orders = OrderSerializer(
+        Order.objects.filter(status = Order.READY, driver = None).order_by("-id"), many = True
+    ).data
+    return JsonResponse({"orders": orders})
+
+@csrf_exempt #as this will be made from outside the system csrf_exempt needs to be used
+#POST request that requires params: access_token, order_id
+def driver_pick_order(request):
+
+        if request.method == "POST":
+            #Get tokena
+            access_token = AccessToken.objects.get(token = request.POST.get("access_token"), expires__gt = timezone.now())
+
+            #Get the driver based on the token ABOVE
+            driver = access_token.user.driver
+
+            #check if driver can only pick up one order at the same time
+            #get all orders assigned to Driver and exclude the ones set to ON THE WAY, this checks to see if the Driver is delivering another order
+            if Order.objects.filter(driver = driver).exclude(status = Order.ONTHEWAY):
+                return JsonResponse ({"status": "failed", "error": "You can only pick one order at a time."})
+
+            try: #search for the Order that is being attempted to be picked up
+                order = Order.objects.get(
+                id = request.POST["order_id"],
+                driver = None,
+                status = Order.READY
+                )
+                order.driver = driver #assign driver to order if it meets the conditions above
+                order.status = Order.ONTHEWAY #update the order status to ONTHEWAY
+                order.picked_at = timezone.now() #update time
+                order.save() #save order
+
+                return JsonResponse({"status": "success"})
+
+            except Order.DoesNotExist:
+                return JsonResponse({"status": "failed", "error": "This order has been picked up by another driver."})
+
+        return JsonResponse({})
+
+#GET params: 'access_token'
+def driver_get_latest_order(request):
+    #Get tokena
+    access_token = AccessToken.objects.get(token = request.GET.get("access_token"), expires__gt = timezone.now())
+
+    #Get the driver based on the token ABOVE
+    driver = access_token.user.driver
+    #Get the order
+    order = OrderSerializer(
+        Order.objects.filter(driver = driver).order_by("picked_at").last() #Get ALL Orders that are asigned to Driver get the last order.
+    ).data
+
+    return JsonResponse({"order": order})
+
+#POST request params: access_token, order_id (note: params for POST requests are passed in the Body of the request)
+@csrf_exempt
+def driver_complete_order(request):
+    #Get token
+    access_token = AccessToken.objects.get(token = request.POST.get("access_token"), expires__gt = timezone.now())
+    #Get the driver based on the token ABOVE
+    driver = access_token.user.driver
+    #Get the order
+    order = Order.objects.get(id = request.POST["order_id"], driver = driver)
+    #Update the order status
+    order.status = Order.DELIVERED
+    order.save()
+
+    return JsonResponse({"status": "success"})
+
+# GET params: access_token
+def driver_get_revenue(request):
+    #Get token
+    access_token = AccessToken.objects.get(token = request.GET.get("access_token"), expires__gt = timezone.now())
+    #Get the driver based on the token ABOVE
+    driver = access_token.user.driver
+
+    from datetime import timedelta #function returns the difference between 2 dates
+
+    revenue = {} #creates an empty dictionary
+    today = timezone.now()
+    current_weekdays = [today + timedelta(days = i) for i in range (0 - today.weekday(), 7 - today.weekday())] #This will return the current weekdays and put into array
+
+    for day in current_weekdays:
+        orders = Order.objects.filter(
+            driver = driver,
+            status = Order.DELIVERED,
+            created_at__year = day.year,
+            created_at__month = day.month,
+            created_at__day = day.day
+        )
+
+        revenue[day.strftime("%a")] = sum(order.total for order in orders) #for all ORDERS in the 'orders' collection, add-up the total, then assign to the day eg TUE, WED
+
+    return JsonResponse({"revenue": revenue}) #return the 'revenue' dictionary
--- foodtaskerapp/serializers.py
+++ foodtaskerapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from foodtaskerapp.models import Restaurant, Meal
+from foodtaskerapp.models import Restaurant, Meal, Customer, Driver, Order, OrderDetails
 
 #Class executed when an API call for getting all restaurants is called
 class RestaurantSerializer(serializers.ModelSerializer):
@@ -30,3 +30,51 @@ class MealSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meal
         fields = ("id", "name", "short_description", "image", "price")
+
+#USED IN ORDER SERIALIZER
+class OrderCustomerSerializer(serializers.ModelSerializer):
+    name = serializers.ReadOnlyField(source="user.get_full_name")
+
+    class Meta:
+        model = Customer
+        fields = ("id", "name", "avatar", "phone", "address")
+
+#USED IN ORDER SERIALIZER
+class OrderDriverSerializer(serializers.ModelSerializer):
+    name = serializers.ReadOnlyField(source="user.get_full_name")
+
+    class Meta:
+        model = Customer
+        fields = ("id", "name", "avatar", "phone", "address")
+
+#USED IN ORDER SERIALIZER
+class OrderRestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ("id", "name", "phone", "address")
+
+#USED IN ORDER SERIALIZER
+class OrderMealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meal
+        fields = ("id", "name", "price")
+
+#USED IN ORDER SERIALIZER
+class OrderDetailsSerializer(serializers.ModelSerializer):
+    meal = OrderMealSerializer
+
+    class Meta:
+        model = OrderDetails
+        fields = ("id", "meal", "quantity", "sub_total")
+
+#USES SERIALIZERS ABOVE
+class OrderSerializer(serializers.ModelSerializer):
+    customer = OrderCustomerSerializer()
+    driver = OrderDriverSerializer()
+    restaurant = OrderRestaurantSerializer()
+    order_details = OrderDetailsSerializer(many = True)
+    status = serializers.ReadOnlyField(source="get_status_display")
+
+    class Meta:
+        model = Order
+        fields = ("id", "customer", "restaurant", "driver", "order_details", "total", "status", "address")
